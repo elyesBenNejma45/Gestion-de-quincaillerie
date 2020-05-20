65b264e92bb81eede2bb188fcabc25013e93f36a
elyesBenNejma45 <elyes.bennejma45@gmail.com>
2020-05-20 02:37:51 +0100

    control access

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/add")
+     */
+    public function addAction()
+    {
+        return $this->render('AppBundle:Security:add.html.twig', array(
+            // ...
+        ));
+    }
+
+    /**
+     * @Route("/admin/show", name="blog_list")
+     */
+    public function redirectAction()
+    {
+        return $this->render('AppBundle:Security:admin_home.html.twig', array(
+            // ...
+        ));
+    }
+
+}
